f516cfc7ae7689876f067dd28f8c4057f492268d
alexrf32 <72366671+alexrf32@users.noreply.github.com>
2024-09-11 20:00:06 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo desde Express!');
+});
+
+// Configuración del puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
